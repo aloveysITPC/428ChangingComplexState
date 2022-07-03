1aeaaf238a44bb437b451cfa704e442a89054b4c
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-07-03 07:10:39 -0400

    mycodeforHello

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 
 function App() {
-  const [fName, getFname] = useState("");
-  const [lName, getLname] = useState("");
+  const [firstName, getFname] = useState("");
+  const [lastName, getLname] = useState("");
 
-  function enterFname() {
-    getFname({ fName });
+  function enterFname(event) {
+    getFname(event.target.value);
   }
-  function enterLname() {
-    getLname({ lName });
+  function enterLname(event) {
+    getLname(event.target.value);
   }
 
   return (
     <div className="container">
       <h1>
-        Hello {fName} {lName}{" "}
+        Hello {firstName} {lastName}
       </h1>
       <form>
         <input onChange={enterFname} name="fName" placeholder="First Name" />
