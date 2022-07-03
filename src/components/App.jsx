bcff4cab49015c18d8f611037fb99d9021dd7217
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-07-03 07:46:02 -0400

    in progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,44 @@
 import React, { useState } from "react";
 
 function App() {
-  const [firstName, getFname] = useState("");
-  const [lastName, getLname] = useState("");
+  // for morecomplex states can set the intitial value to an object
+  const [fullName, getFullname] = useState({ firstName: "", lastName: "" });
 
-  function enterFname(event) {
-    getFname(event.target.value);
-  }
-  function enterLname(event) {
-    getLname(event.target.value);
+  function enterFullName(event) {
+    const newValue = event.target.value;
+    // use the name property of the input box
+    const inputName = event.target.name;
+    console.log(inputName);
+    console.log(newValue);
+    // pass a function to a function - functional programming
+    // Don't use events inside a setState function
+    getFullname((prevValue) => {
+      console.log(prevValue);
+    });
   }
 
   return (
     <div className="container">
       <h1>
-        Hello {firstName} {lastName}
+        Hello {fullName.firstName}
+        {fullName.lastName}
       </h1>
       <form>
         {/* remember events for forms */}
+        {/* controlled components because the value is set */}
         <input
-          onChange={enterFname}
+          onChange={enterFullName}
+          // have the property name
           name="fName"
           placeholder="First Name"
           // be sure to add value to the form
-          value={firstName}
+          // value={fullName.firstName}
         />
         <input
-          onChange={enterLname}
+          onChange={enterFullName}
           name="lName"
           placeholder="Last Name"
-          value={lastName}
+          // value={fullName.lastName}
         />
         <button>Submit</button>
       </form>
